93007ab20ba974960ad415cefccdc4030e1f9028
Bolo_fang <bfang@DCNA9133.pc.cn.t-internal.com>
2017-05-15 20:53:11 +0800

    init commit

---
+++ IBIntegration/src/main/java/com/tsystems/si/aviation/imf/ibintegration/IbIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.tsystems.si.aviation.imf.ibintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbIntegrationApplication.class, args);
+	}
+}
